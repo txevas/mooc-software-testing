614d19b0debd6ed38b8f8df95a3060a2f182cb38
jgervas <jordi.gervas@gmail.com>
2018-11-07 11:50:48 +0100

    Completed the test class for addFive2

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -15,13 +15,13 @@ public class GettingStartedTest {
 
     @Test
     public void addFiveToZero() {
-        int result = new GettingStarted().addFive(???);
-        Assertions.assertEquals(???, result);
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
     }
 
     @Test
     public void addFiveToMinus20() {
-        int result = new GettingStarted().addFive(???);
-        Assertions.assertEquals(????,result);
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
     }
 }
